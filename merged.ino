35a6fcaefac7876b7eeb291419826e9915b29685
BatrinutSilviu <43755543+BatrinutSilviu@users.noreply.github.com>
2019-01-30 19:39:03 +0200

    Add files via upload

--- merged.ino
+++ merged.ino
@@ -42,22 +42,33 @@ void loop()
         sprintf(buf, "  block %d: ", j);
         //Serial.println(buf);
         //pixy.blocks[j].print();         // in for u asta putem accesa x,y si altele prin pixy.blocks[j]
-        if(pixy.blocks[j].x < 160 )
+        if(pixy.blocks[j].x < 106 )
         {
-          sutDreptu();
-            delay(500);
+          turnStanga();
+          delay(1000);
+          PasDupaTurnStanga();
+          delay(1000);
           pozInitial();
-          } 
-          else
+         } 
+         else 
+         {
+          if(pixy.blocks[j].x >=106 && pixy.blocks[j].x < 213)
           {
             miscatiHoitu();
             pozInitial();
-            }
+          }
+          else
+          {
+            turnDreapta();
+            delay(1000);
+            PasDupaTurnDreapta();
+            delay(1000);
+            pozInitial();
+           }
+         }
       }
     }
   } 
-  
-  //miscatiHoitu();
 }
 
 void miscatiHoitu()
@@ -119,7 +130,58 @@ void sutDreptu()
   Serial.println("#2 P1500 T500");
 }
 
-void checkPixy()
+void turnDreapta()
 {
+  Serial.println("#16 P1475 T500");
+  Serial.println("#0 P1520 T500");
   
+  Serial.println("#18 P1350 T500");     //de aici putem intoarce mai mult sau mai putin,astea 4 comenzi
+  Serial.println("#17 P1050 T500");     
+  
+  Serial.println("#1 P1250 T500");
+  Serial.println("#2 P1200 T350");
+}
+
+void turnStanga()
+{
+  Serial.println("#16 P1475 T500");
+  Serial.println("#0 P1520 T500");
+  
+  Serial.println("#18 P1750 T500");     //de aici putem intoarce mai mult sau mai putin,astea 4 comenzi
+  Serial.println("#17 P1650 T500");     
+  
+  Serial.println("#1 P1850 T500");
+  Serial.println("#2 P1800 T350");
+}
+
+void PasDupaTurnStanga()
+{
+  Serial.println("#0 P1620 T500");
+  Serial.println("#16 P1575 T500");
+  delay(500);
+  
+  Serial.println("#1 P1400 T500");
+  Serial.println("#2 P1350 T500");
+  Serial.println("#17 P1200 T500");
+  Serial.println("#18 P1400 T500");
+  delay(1000);
+  Serial.println("#0 P1520 T500");
+  Serial.println("#16 P1475 T500");
+  delay(1000);
+}
+
+void PasDupaTurnDreapta()
+{
+  Serial.println("#0 P1420 T500");
+  Serial.println("#16 P1375 T500");
+  delay(500);
+
+  Serial.println("#1 P1700 T500");
+  Serial.println("#2 P1550 T500");
+  Serial.println("#17 P1500 T500");
+  Serial.println("#18 P1700 T500");
+  delay(1000);
+  Serial.println("#0 P1520 T500");
+  Serial.println("#16 P1475 T500");
+  delay(1000);
 }
