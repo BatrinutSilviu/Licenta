e8e84b520d5809c6540521d311ae5561035fa695
BatrinutSilviu <43755543+BatrinutSilviu@users.noreply.github.com>
2019-02-12 19:59:29 +0200

    Add files via upload

--- merged.ino
+++ merged.ino
@@ -2,7 +2,7 @@
 #include <Pixy.h>
 #include <LiquidCrystal.h>
 
-#define TimpMediu "T500"
+#define TimpMediu "T250"
 
 Pixy pixy;
 
@@ -53,7 +53,7 @@ void loop()
          {
           if(pixy.blocks[blockIterator].x >= 106 && pixy.blocks[blockIterator].x < 213)
           {
-            if(pixy.blocks[blockIterator].y > 125 )
+            if(pixy.blocks[blockIterator].y > 125)
             {
               miscatiHoitu();
               miscatiHoitu();
@@ -91,134 +91,135 @@ void loop()
 void miscatiHoitu()
 {
   //inclinare talpii
-  Serial.println("#0 P1620 T500");
-  Serial.println("#16 P1575 T500");
+  Serial.println("#0 P1620 T250");
+  Serial.println("#16 P1575 T250");
   delay(500);
   //primu pas
-  Serial.println("#1 P1400 T500");
-  Serial.println("#2 P1350 T500");
-  Serial.println("#17 P1200 T500");
-  Serial.println("#18 P1400 T500");
-  delay(1000);
-  Serial.println("#0 P1520 T500");
-  Serial.println("#16 P1475 T500");
-  delay(1000);
-  //al doilea pas
-  Serial.println("#0 P1460 T500");  //1420
-  Serial.println("#16 P1375 T500");
+  Serial.println("#1 P1400 T250");
+  Serial.println("#2 P1350 T250");
+  Serial.println("#17 P1200 T250");
+  Serial.println("#18 P1400 T250");
   delay(500);
-  Serial.println("#1 P1700 T500");
-  Serial.println("#2 P1650 T500");
-  Serial.println("#17 P1500 T500");
-  Serial.println("#18 P1700 T500");
-  delay(1000);
-  Serial.println("#0 P1520 T500");
-  Serial.println("#16 P1475 T500");
-  delay(1000);
+  Serial.println("#0 P1520 T250");
+  Serial.println("#16 P1475 T250");
+  delay(500);
+  //al doilea pas
+  Serial.println("#0 P1460 T250");  //1420
+  Serial.println("#16 P1375 T250");
+  delay(500);
+  Serial.println("#1 P1700 T250");
+  Serial.println("#2 P1650 T250");
+  Serial.println("#17 P1500 T250");
+  Serial.println("#18 P1700 T250");
+  delay(500);
+  Serial.println("#0 P1520 T250");
+  Serial.println("#16 P1475 T250");
+  delay(500);
 }
 
 void pozInitial()
 {
-  Serial.println("#0 P1520 T500");
-  Serial.println("#16 P1475 T500");
-  Serial.println("#1 P1550 T500");
-  Serial.println("#2 P1500 T500");
-  Serial.println("#17 P1350 T500");
-  Serial.println("#18 P1550 T500");
+  Serial.println("#0 P1520 T250");
+  Serial.println("#16 P1475 T250");
+  Serial.println("#1 P1550 T250");
+  Serial.println("#2 P1500 T250");
+  Serial.println("#17 P1350 T250");
+  Serial.println("#18 P1550 T250");
 }
+
 
 void sutDreptu()
 {
-  Serial.println("#17 P1350 T500");
-  Serial.println("#18 P1550 T500");
-  Serial.println("#16 P1650 T500");
-  Serial.println("#0 P1800 T500");
-  delay(1000);
+  Serial.println("#17 P1350 T350");
+  Serial.println("#18 P1550 T350");
+  Serial.println("#16 P1650 T350");
+  Serial.println("#0 P1800 T350");
+  delay(750);
 
-  Serial.println("#1 P1750 T500");
-  Serial.println("#2 P2000 T350");
-  Serial.println("#0 P1520 T500");
-  delay(1000);
+  Serial.println("#1 P1750 T350");
+  Serial.println("#2 P2000 T350");      //aici mai rpd
+  Serial.println("#0 P1520 T350");
+  delay(750);
 
-  Serial.println("#1 P1750 T500");
-  Serial.println("#2 P1000 T350");
-  delay(500);
-  Serial.println("#1 P1550 T500");
-  Serial.println("#2 P1500 T500");
+  Serial.println("#1 P1750 T350");
+  Serial.println("#2 P1000 T350");      //aici mai rpd
+  delay(750);
+  Serial.println("#1 P1550 T350");
+  Serial.println("#2 P1500 T350");
 }
 
 void turnDreapta()
 {
-  Serial.println("#16 P1475 T500");
-  Serial.println("#0 P1520 T500");
+  Serial.println("#16 P1475 T250");
+  Serial.println("#0 P1520 T250");
   
-  Serial.println("#18 P1350 T500");     //de aici putem intoarce mai mult sau mai putin,astea 4 comenzi
-  Serial.println("#17 P1050 T500");     
+  Serial.println("#18 P1350 T250");     //de aici putem intoarce mai mult sau mai putin,astea 4 comenzi
+  Serial.println("#17 P1050 T250");     
   
-  Serial.println("#1 P1250 T500");
-  Serial.println("#2 P1200 T350");
+  Serial.println("#1 P1250 T250");
+  Serial.println("#2 P1200 T250");      //aici mai rpd
 }
 
 void turnStanga()
 {
-  Serial.println("#16 P1475 T500");
-  Serial.println("#0 P1520 T500");
+  Serial.println("#16 P1475 T250");
+  Serial.println("#0 P1520 T250");
   
-  Serial.println("#18 P1750 T500");     //de aici putem intoarce mai mult sau mai putin,astea 4 comenzi
-  Serial.println("#17 P1650 T500");     
+  Serial.println("#18 P1750 T250");     //de aici putem intoarce mai mult sau mai putin,astea 4 comenzi
+  Serial.println("#17 P1650 T250");     
   
-  Serial.println("#1 P1850 T500");
-  Serial.println("#2 P1800 T350");
+  Serial.println("#1 P1850 T250");
+  Serial.println("#2 P1800 T250");      //aici mai rpd
 }
 
 void PasDupaTurnStanga()
 {
-  Serial.println("#0 P1620 T500");
-  Serial.println("#16 P1575 T500");
+  Serial.println("#0 P1620 T250");
+  Serial.println("#16 P1575 T250");
   delay(500);
   
-  Serial.println("#1 P1400 T500");
-  Serial.println("#2 P1350 T500");
-  Serial.println("#17 P1200 T500");
-  Serial.println("#18 P1400 T500");
-  delay(1000);
-  Serial.println("#0 P1520 T500");
-  Serial.println("#16 P1475 T500");
-  delay(1000);
+  Serial.println("#1 P1400 T250");
+  Serial.println("#2 P1350 T250");
+  Serial.println("#17 P1200 T250");
+  Serial.println("#18 P1400 T250");
+  delay(500);
+  Serial.println("#0 P1520 T250");
+  Serial.println("#16 P1475 T250");
+  delay(500);
 }
 
 void PasDupaTurnDreapta()
 {
-  Serial.println("#0 P1420 T500");
-  Serial.println("#16 P1375 T500");
+  Serial.println("#0 P1420 T250");
+  Serial.println("#16 P1375 T250");
   delay(500);
 
-  Serial.println("#1 P1700 T500");
-  Serial.println("#2 P1550 T500");
-  Serial.println("#17 P1500 T500");
-  Serial.println("#18 P1700 T500");
-  delay(1000);
-  Serial.println("#0 P1520 T500");
-  Serial.println("#16 P1475 T500");
-  delay(1000);
+  Serial.println("#1 P1700 T250");
+  Serial.println("#2 P1550 T250");
+  Serial.println("#17 P1500 T250");
+  Serial.println("#18 P1700 T250");
+  delay(500);
+  Serial.println("#0 P1520 T250");
+  Serial.println("#16 P1475 T250");
+  delay(500);
 }
 
 void PasMare()
 {
   turnStanga();
-  delay(1000);
+  delay(500);
   PasDupaTurnStanga();
-  delay(1000);
+  delay(500);
   
   turnStanga();
-  delay(1000);
+  delay(500);
   PasDupaTurnStanga();
-  delay(1000);
+  delay(500);
   
   turnStanga();
-  delay(1000);
+  delay(500);
   PasDupaTurnStanga();
-  delay(1000);
+  delay(500);
   
   pozInitial();  
 }
