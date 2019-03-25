0780c923482296da4b0f2c446e15da9eeba9e582
BatrinutSilviu <43755543+BatrinutSilviu@users.noreply.github.com>
2019-03-25 15:38:12 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -13,191 +13,78 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private boolean ManAut=false;
+    private boolean ManAut = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void apasareButonManual(View v)
-    {
-        final TextView textView = (TextView) findViewById(R.id.tv);
+
+    public void apasareButonManual(View v) {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url;
-        if(ManAut == false) {
+        if (ManAut == false) {
             url = "http://192.168.1.126/2/on";
-        }
-        else
-        {
+        } else {
             url = "http://192.168.1.126/2/off";
         }
-
-// Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
             }
         });
-        if(ManAut ==false) {
-            ManAut = true;
+        ManAut = !ManAut;
+        queue.add(stringRequest);
+    }
+
+    public void apasareButonUniversal(View v) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "";
+        switch (v.getId()) {
+            case (R.id.fata): {
+                url = "http://192.168.1.126/5/on";
+                break;
+            }
+            case (R.id.spate): {
+                url = "http://192.168.1.126/15/on";
+                break;
+            }
+            case (R.id.stanga): {
+                url = "http://192.168.1.126/18/on";
+                break;
+            }
+            case (R.id.dreapta): {
+                url = "http://192.168.1.126/23/on";
+                break;
+            }
+            case (R.id.sutdrept): {
+                url = "http://192.168.1.126/19/on";
+                break;
+            }
+            case (R.id.sutstangu): {
+                url = "http://192.168.1.126/22/on";
+                break;
+            }
         }
-        else
-        {
-            ManAut=false;
-        }
-        queue.add(stringRequest);
-    }
-
-    public void apasareButonFata(View v)
-    {
-        final TextView textView = (TextView) findViewById(R.id.tv);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.1.126/5/on";
-
-// Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
             }
         });
-
-        queue.add(stringRequest);
-    }
-
-    public void apasareButonSpate(View v)
-    {
-        final TextView textView = (TextView) findViewById(R.id.tv);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.1.126/15/on";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-        queue.add(stringRequest);
-    }
-
-    public void apasareButonStanga(View v)
-    {
-        final TextView textView = (TextView) findViewById(R.id.tv);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.1.126/18/on";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-        queue.add(stringRequest);
-    }
-    public void apasareButonDreapta(View v)
-    {
-        final TextView textView = (TextView) findViewById(R.id.tv);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.1.126/23/on";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-        queue.add(stringRequest);
-    }
-
-    public void apasareButonSutDreptu(View v)
-    {
-        final TextView textView = (TextView) findViewById(R.id.tv);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.1.126/19/on";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
-        queue.add(stringRequest);
-    }
-
-    public void apasareButonSutStangu(View v)
-    {
-        final TextView textView = (TextView) findViewById(R.id.tv);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.1.126/22/on";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
-
         queue.add(stringRequest);
     }
 }
